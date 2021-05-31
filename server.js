b244594554ad4e3ef08d06416e172d1e6ad7e808
exauce-mwililikwa <exaucemwililikwa@gmail.com>
2021-05-31 23:33:58 +0200

    adding dependecies

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var server=express();
  
 server.get('/', function (req, res){
 res.setHeader('Content-Type','text/html');
-res.status(200).send('<h1> bonour sur mon serveur </h1>');
+res.status(200).send('<h1> Bonjour sur mon serveur </h1>');
 });
 server.listen(8080,function(){
-    console.log('Server en ecoute');
+    console.log('Server en ecoute ...');
 });
