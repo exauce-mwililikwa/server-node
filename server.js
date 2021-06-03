9310a6da4dea9e7f83dc4313da6ecc091ccc68ae
exauce-mwililikwa <exaucemwililikwa@gmail.com>
2021-06-04 00:24:39 +0200

    dependencies

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ server.get('/', function (req, res){
 res.setHeader('Content-Type','text/html');
 res.status(200).send('<h1> Bonjour sur mon serveur </h1>');
 });
-server.listen(8080,function(){
+server.listen(8081,function(){
     console.log('Server en ecoute ...');
 });
